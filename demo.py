e976f147c4615eccfc83b52e93804078b03f1a44
boai <sunboyan@outlook.com>
2017-03-07 16:57:25 +0800

    python 基本语法练习
    Signed-off-by: boai <sunboyan@outlook.com>

---
+++ demo.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env 
+# -*- coding:UTF-8 -*-
+
+# print("hello, world!")
+# print("hello, world1", "123")
+# name = input("Please enter your name：")
+# print("hello", name)
+
+
+# a = input('请输入变量a：')
+# b = input('请输入变量b：')
+# print("a + b = ", a + b)
+# 请输入变量a：1024
+# 请输入变量b：2
+# a + b =  10242
+
+
+# a = 100
+# if a % 2 == 0:
+# 	print('你输入的是2的倍数！')
+# else:
+# 	print('你输入的不是2的倍数！')
+
+
+# 转义字符\可以转义很多字符，比如\n表示换行，\t表示制表符，字符\本身也要转义，所以\\表示的字符就是\，可以在Python的交互式命令行用print()打印字符串看看：
+# print('I\'m OK !')
+# print('''...line1...line2''')
+# print('''line1
+# ... line2
+# ... line3''')
+# print('''line1
+# line2
+# line3''')
+
+
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# boai!'''
+
+# print(n, f, s1, s2, s3, s4)
+
+# if True:
+# 	print('True')
+# else:
+# 	print('False')
+	
+ # Fibonacci series: 斐波纳契数列
+ # 两个元素的总和确定了下一个数
+# a, b = 0, 1
+# print(a, b)
+
+# a = 4.3-1
+# print(a)
+# b = 2**3
+# print(b)
+
+
+# 与C字符串不同的是，Python字符串不能被改变。向一个索引位置赋值，比如word[0] = 'm'会导致错误。
+# 注意：
+# 1、反斜杠可以用来转义，使用r可以让反斜杠不发生转义。
+# 2、字符串可以用+运算符连接在一起，用*运算符重复。
+# 3、Python中的字符串有两种索引方式，从左往右以0开始，从右往左以-1开始。
+# 4、Python中的字符串不能改变。
+
+# string = "我是博爱！"
+# print(string[1], string[3])
+string = "I love Python!"
+print(string[2:6])
+print(string[-12:-8])
+
+string2 = "：boai"
+print(string + string2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
