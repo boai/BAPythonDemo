283a9098b8904919bd07fbac548fd18b7a319f81
boai <sunboyan@outlook.com>
2017-10-23 18:02:13 +0800

    子类、父类、继承、私有变量 demo

--- Python 学习 demo/demo3.py
+++ Python 学习 demo/demo3.py
@@ -1,3 +1,45 @@
 
 
+class BAParent:
+    parentAttr = 100
+
+    __money = 20
+
+    def __init__(self):
+        print("调用父类构造函数")
+
+    def parentMethod(self):
+        print('调用父类方法')
+
+    def setAttr(self, attr):
+        BAParent.parentAttr = attr
+
+    def getAttr(self):
+        print("父类属性 :", BAParent.parentAttr)
+
+    def testFunction(self):
+        print('调用父类方法')
+
+class BAChild(BAParent):
+
+    def __init__(self):
+        print("调用子类构造方法")
+
+    def childMethod(self):
+        print('调用子类方法 child method')
+
+    def testFunction(self):
+        print('调用子类方法')
+
+
+c = BAChild()        # 实例化子类
+c.childMethod()      # 调用子类的方法
+c.parentMethod()     # 调用父类方法
+c.setAttr(200)       # 再次调用父类的方法
+c.getAttr()          # 再次调用父类的方法
+
+c.testFunction()
+
+print('私有变量：', c.__money)
+
 
