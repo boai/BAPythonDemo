83630c7e0688807006af482baca5adf9a1c7f437
boai <sunboyan@outlook.com>
2017-10-25 09:35:18 +0800

    新增CGI demo

---
+++ Python 学习 demo/cgiDemo.py
@@ -0,0 +1,14 @@
+
+#coding=utf-8
+#!/usr/bin/python
+
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello Word - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello Word! This is boai first CGI program</h2>')
+print ('</body>')
+print ('</html>')
+
