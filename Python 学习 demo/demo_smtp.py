01ea6917e96a5c75e7459faaba2b1e97e0f20cf6
boai <sunboyan@outlook.com>
2017-10-25 14:21:22 +0800

    新增 Python 邮件发送demo封装

--- Python 学习 demo/demo_smtp.py
+++ Python 学习 demo/demo_smtp.py
@@ -1,46 +1,53 @@
+
 import smtplib
+
 from email.header import Header
 from email.mime.text import MIMEText
 
 # 第三方 SMTP 服务
-mail_host = "smtp.qq.com"      # SMTP服务器
-mail_user = "137361770@qq.com"                  # 用户名
-mail_pass = "qhbibauvewgebjbi"               # 授权密码，非登录密码
+# SMTP服务器
+mail_host = 'smtp.qq.com'
+# 用户名
+mail_user = '137361770@qq.com'
+# 授权密码，非登录密码
+mail_pwd = 'qhbibauvewgebjbi'
 
-sender = '137361770@qq.com'    # 发件人邮箱(最好写全, 不然会失败)
-receivers = ['sunboyan@outlook.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+# 发件人邮箱(最好写全, 不然会失败)
+sender = '137361770@qq.com'
+# 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+# receivers = ['sunboyan@outlook.com', '1299625033@qq.com']
+receivers = ['sunboyan@outlook.com']
 
-content = '我用Python'
-title = '这里是Python 发送邮件的title'  # 邮件主题
+mail_title = 'Email test by Python'
+mail_content = 'welcome to boaihome!'
 
-def sendEmail():
+def ba_smtp_sendEmail(email_title, email_content, email_sender, email_receiver, email_host, email_user, email_pwd):
 
-    message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
-    message['From'] = "{}".format(sender)
-    message['To'] = ",".join(receivers)
-    message['Subject'] = title
+    # 内容, 格式, 编码
+    message = MIMEText(email_content, 'plain', 'utf-8')
+    message['From'] = '{}'.format(email_sender)
+    message['To'] = ','.join(email_receiver)
+    message['Subject'] = email_title
 
     try:
-        smtpObj = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口一般是465
-        smtpObj.login(mail_user, mail_pass)  # 登录验证
-        smtpObj.sendmail(sender, receivers, message.as_string())  # 发送
-        print("mail has been send successfully.")
+        # 启用SSL发信, 端口一般是465
+        smtpObj = smtplib.SMTP_SSL(email_host, 465)
+        # 登录验证
+        smtpObj.login(email_user, email_pwd)
+        # 发送
+        smtpObj.sendmail(email_sender, email_receiver, message.as_string())
+        print('mail has been send successfully!')
     except smtplib.SMTPException as e:
+        print("Error: 无法发送邮件")
         print(e)
 
-def send_email2(SMTP_host, from_account, from_passwd, to_account, subject, content):
-    email_client = smtplib.SMTP(SMTP_host)
-    email_client.login(from_account, from_passwd)
-    # create msg
-    msg = MIMEText(content, 'plain', 'utf-8')
-    msg['Subject'] = Header(subject, 'utf-8')  # subject
-    msg['From'] = from_account
-    msg['To'] = to_account
-    email_client.sendmail(from_account, to_account, msg.as_string())
-
-    email_client.quit()
 
 if __name__ == '__main__':
-    sendEmail()
-    # receiver = '***'
-    # send_email2(mail_host, mail_user, mail_pass, receiver, title, content)
+
+    i = 0
+    while i < 3:
+        title = ('%s%d'%(mail_title, i))
+        ba_smtp_sendEmail(title, mail_content, sender, receivers, mail_host,mail_user, mail_pwd)
+        i += 1
+    print('邮件发送完毕！结束循环！')
+
