dcbde9d2c252c5d1fba6a15ef769653791a0f33d
boai <sunboyan@outlook.com>
2017-11-02 10:40:37 +0800

    新增 格式化抓取数据的demo，

--- Python 学习 demo/Beautiful Soup/demo_BeautifulSoup.py
+++ Python 学习 demo/Beautiful Soup/demo_BeautifulSoup.py
@@ -156,35 +156,260 @@ if type(soup.a.string) == bs4.element.Comment:
     print('soup.a.string 类型2：', type(soup.a.string))
 
 
+# 6. 遍历文档树
+'''
+tag 的 .content 属性可以将tag的子节点以列表的方式输出
+输出方式为列表，我们可以用列表索引来获取它的某一个元素
+
+soup.head.contents： [<title>The Dormouse's story</title>]
+soup.head.contents[0]： <title>The Dormouse's story</title>
+'''
+print('soup.head.contents：', soup.head.contents)
+print('soup.head.contents[0]：', soup.head.contents[0])
+
+'''
+.children
+
+它返回的不是一个 list，不过我们可以通过遍历获取所有子节点。
+
+我们打印输出 .children 看一下，可以发现它是一个 list 生成器对象,
+我们怎样获得里面的内容呢？很简单，遍历一下就好了，代码及结果如下
+
+soup.head.children： <list_iterator object at 0x1046e42e8>
+child： <title>The Dormouse's story</title>
+'''
+print('soup.head.children：', soup.head.children)
+for child in soup.head.children:
+    print('child：', child)
+
+'''
+body.children： 
+
+body.children： <p class="boaiClass" name="boaihome"><b>The Dormouse's story</b></p>
+body.children： 
+
+body.children： <p class="story">Once upon a time there were three little sisters; and their names were
+<a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>,
+<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and
+<a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+body.children： 
+
+body.children： <p class="story">...</p>
+body.children： 
+'''
+# for child in soup.body.children:
+    # print('body.children：', child)
+
+'''
+soup.descendants： <html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="boaiClass" name="boaihome"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>,
+<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and
+<a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+</body></html>
+soup.descendants： <head><title>The Dormouse's story</title></head>
+soup.descendants： <title>The Dormouse's story</title>
+soup.descendants： The Dormouse's story
+soup.descendants： 
+
+soup.descendants： <body>
+<p class="boaiClass" name="boaihome"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>,
+<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and
+<a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+</body>
+soup.descendants： 
+
+soup.descendants： <p class="boaiClass" name="boaihome"><b>The Dormouse's story</b></p>
+soup.descendants： <b>The Dormouse's story</b>
+soup.descendants： The Dormouse's story
+soup.descendants： 
+
+soup.descendants： <p class="story">Once upon a time there were three little sisters; and their names were
+<a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>,
+<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and
+<a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+soup.descendants： Once upon a time there were three little sisters; and their names were
+
+soup.descendants： <a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>
+soup.descendants：  Elsie 
+soup.descendants： ,
+
+soup.descendants： <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>
+soup.descendants： Lacie
+soup.descendants：  and
+
+soup.descendants： <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+soup.descendants： Tillie
+soup.descendants： ;
+and they lived at the bottom of a well.
+soup.descendants： 
+
+soup.descendants： <p class="story">...</p>
+soup.descendants： ...
+soup.descendants： 
+'''
+# for child in soup.descendants:
+    # print('soup.descendants：', child)
+
+'''
+.strings
+
+获取多个内容，不过需要遍历获取，比如下面的例子
+
+soup.strings：
+ The Dormouse's story
+soup.strings：
+ 
+
+soup.strings：
+ 
+
+soup.strings：
+ The Dormouse's story
+soup.strings：
+ 
+
+soup.strings：
+ Once upon a time there were three little sisters; and their names were
+
+soup.strings：
+ ,
+
+soup.strings：
+ Lacie
+soup.strings：
+  and
+
+soup.strings：
+ Tillie
+soup.strings：
+ ;
+and they lived at the bottom of a well.
+soup.strings：
+ 
+
+soup.strings：
+ ...
+soup.strings：
+'''
+# for string in soup.strings:
+#     print('soup.strings：\n', string)
+
+'''
+.stripped_strings
+
+输出的字符串中可能包含了很多空格或空行,使用 .stripped_strings 可以去除多余空白内容
+
+soup.strings：
+ The Dormouse's story
+soup.strings：
+ The Dormouse's story
+soup.strings：
+ Once upon a time there were three little sisters; and their names were
+soup.strings：
+ ,
+soup.strings：
+ Lacie
+soup.strings：
+ and
+soup.strings：
+ Tillie
+soup.strings：
+ ;
+and they lived at the bottom of a well.
+soup.strings：
+ ...
+'''
+# for string in soup.stripped_strings:
+#     print('soup.strings：\n', string)
+
+'''
+父节点
+
+知识点： .parent 属性
+
+soup.p.parent.name： body
+soup.head.title.string.parent.name： title
+'''
+print('soup.p.parent.name：', soup.p.parent.name)
+print('soup.head.title.string.parent.name：', soup.head.title.string.parent.name)
+
+'''
+全部父节点
+
+知识点：.parents 属性
+通过元素的 .parents 属性可以递归得到元素的所有父辈节点，例如
+
+soup.head.title.string.parents： title
+soup.head.title.string.parents： head
+soup.head.title.string.parents： html
+soup.head.title.string.parents： [document]
+'''
+# for parent in soup.head.title.string.parents:
+#     print('soup.head.title.string.parents：', parent.name)
 
 
+'''
+兄弟节点
 
+知识点：.next_sibling .previous_sibling 属性
+兄弟节点可以理解为和本节点处在统一级的节点，
+.next_sibling 属性获取了该节点的下一个兄弟节点，
+.previous_sibling 则与之相反，如果节点不存在，则返回 None
 
+注意：实际文档中的tag的 .next_sibling 和 .previous_sibling 属性通常是字符串或空白，
+因为空白或者换行也可以被视作一个节点，所以得到的结果可能是空白或者换行
 
+soup.p.next_sibling： 
+#       实际该处为空白
+soup.p.prev_sibling： None
+soup.p.next_sibling.next_sibling： 
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>,
+<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and
+<a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
 
+'''
+# print('soup.p.next_sibling：', soup.p.next_sibling)
+# print('soup.p.prev_sibling：', soup.p.prev_sibling)
+# print('soup.p.next_sibling.next_sibling：', soup.p.next_sibling.next_sibling)
 
+'''
+全部兄弟节点
 
+知识点：.next_siblings .previous_siblings 属性
+通过 .next_siblings 和 .previous_siblings 属性可以对当前节点的兄弟节点迭代输出
 
+soup.a.next_siblings： ',\n'
+soup.a.next_siblings： <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>
+soup.a.next_siblings： ' and\n'
+soup.a.next_siblings： <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+soup.a.next_siblings： ';\nand they lived at the bottom of a well.'
 
+'''
+# for sibling in soup.a.next_siblings:
+#     print('soup.a.next_siblings：', repr(sibling))
 
+'''
+前后节点
 
+知识点：.next_element .previous_element 属性
+与 .next_sibling .previous_sibling 不同，它并不是针对于兄弟节点，而是在所有节点，不分层次
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+比如 head 节点为
+'''
+print('soup.head.next_element：', soup.head.next_element)
 
 
 
